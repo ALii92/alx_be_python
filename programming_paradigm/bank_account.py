747dfa528aede4271fd449c8f4d47c8c17595687
ALii92 <153398483+ALii92@users.noreply.github.com>
2024-07-14 23:51:27 +0100

    Update bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,18 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
-
+    def __init__(self, account_balance ) :
+        self.account_balance = account_balance 
+        # account_balance = 0
     def deposit(self, amount):
+        # amount = int(input("Enter the amount to deposit: "))
         self.account_balance += amount
-
     def withdraw(self, amount):
+        # amount = int(input("Enter the amount to withdraw: "))
         if self.account_balance >= amount:
+            
             self.account_balance -= amount
             return True
-        else:
+        else :
             return False
-
-    def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        
+    def display_balance (self):
+        print(f"Current Balance: ${self.account_balance:.2f}")
